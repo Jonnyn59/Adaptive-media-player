61d4a358ad35c592ba4dcd98436efece8483a989
котяра <94602071+Gamerkot911@users.noreply.github.com>
2024-11-01 18:24:30 +0300

    -

--- src/Login.tsx
+++ src/Login.tsx
@@ -8,21 +8,13 @@ function login(){
 function Login() {
     return (
         <div id={"logpage"}>
-            <div className={"inputBox llogPos"}>
-                <input type={"text"} required={true}/>
-                <span>Login</span>
-            </div>
-            <div className={"inputBox ppassPos"}>
-                <input type={"text"} required={true}/>
-                <span>Password</span>
+            <div className={"customTestDiv"}>
+                <input className={"customTest"} type={"text"}/>
+                <span>LOGIN</span>
             </div>
             <button className={"ui-btn"}>
                 <span>_____LOGIN_____</span><i></i>
             </button>
-            <div className={"customTestDiv"}>
-                <span>LOGIN</span>
-                <input className={"customTest"} type={"text"}/>
-            </div>
         </div>
     )
 }
